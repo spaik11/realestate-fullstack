29e53297ada2313d162805d41d16d9d232f105cf
phenix1229 <paul.garay@codeimmersives.com>
2020-06-08 11:06:03 -0400

    fixed css to hilight active list in sidebar

--- client/src/components/MapPage/MapPage.js
+++ client/src/components/MapPage/MapPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles, Grid } from '@material-ui/core';
-import Sidebar from './Sidebar';
+import Sidebar from '../Sidebar/Sidebar';
 import './MapPage.css';
 
 
