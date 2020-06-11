5ae8dcd2228d806a694767051f2b13ba07a01786
Sung Paik <sung.paik@codeimmersives.com>
2020-06-11 11:45:59 -0400

    added active property context and setup api call and a conditional css rendering on the map page

--- client/src/components/MapPage/Map/Map.js
+++ client/src/components/MapPage/Map/Map.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { CityContext } from "../../Context/CityContext";
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import { Icon } from "leaflet";
 import "./Map.css";
@@ -8,20 +9,10 @@ export const icon = new Icon({
   iconSize: [25, 25],
 });
 
-export default function LeafMap({ data, city }) {
-  const [activeProp, setActiveProp] = useState(null);
-
-  const addCommas = (nStr) => {
-    nStr += "";
-    let x = nStr.split(".");
-    let x1 = x[0];
-    let x2 = x.length > 1 ? "." + x[1] : "";
-    let rgx = /(\d+)(\d{3})/;
-    while (rgx.test(x1)) {
-      x1 = x1.replace(rgx, "$1" + "," + "$2");
-    }
-    return x1 + x2;
-  };
+export default function LeafMap({ data, city, addCommas }) {
+  const { activeProp, setActiveProp } = useContext(CityContext);
+  // const [activeProp, setActiveProp] = useState(null);
+  console.log("MAP", activeProp);
 
   let mapCenter = data.filter(({ Address }) =>
     Address.split(" ").includes(city)
@@ -34,7 +25,7 @@ export default function LeafMap({ data, city }) {
           ? [30.2672, -97.7431]
           : [mapCenter.Latitude, mapCenter.Longitude]
       }
-      zoom={13}>
+      zoom={10}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
--- client/src/components/MapPage/MapPage.js
+++ client/src/components/MapPage/MapPage.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect, useContext } from "react";
 import { CityContext } from "../Context/CityContext";
 import { makeStyles, Grid } from "@material-ui/core";
 import Sidebar from "./Sidebar/Sidebar";
-import texasProperty from "../../lib/API/texas.json";
+import { fetchRealEstateData } from "../../lib/api/api";
+import texasProperty from "../../lib/api/texas.json";
 import Map from "./Map/Map";
 import "./MapPage.css";
 
@@ -18,19 +19,41 @@ export default function MapPage() {
   const { currentCity } = useContext(CityContext);
 
   useEffect(() => {
-    setApiProperty(texasProperty);
-    console.log(currentCity);
-  }, [currentCity]);
+    // setApiProperty(texasProperty);
+    callApi();
+  }, []);
+
+  const callApi = async () => {
+    let apiData = await fetchRealEstateData();
+    console.log(apiData);
+    setApiProperty(apiData);
+  };
 
   const classes = useStyles();
+
+  const addCommas = (nStr) => {
+    nStr += "";
+    let x = nStr.split(".");
+    let x1 = x[0];
+    let x2 = x.length > 1 ? "." + x[1] : "";
+    let rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+      x1 = x1.replace(rgx, "$1" + "," + "$2");
+    }
+    return x1 + x2;
+  };
 
   return (
     <Grid container id="main" className={classes.root} spacing={2}>
       <Grid item id="map">
-        <Map data={apiProperty} city={currentCity} />
+        <Map data={apiProperty} city={currentCity} addCommas={addCommas} />
       </Grid>
       <Grid item id="sidebar">
-        <Sidebar data={apiProperty} setList={setApiProperty} />
+        <Sidebar
+          data={apiProperty}
+          setList={setApiProperty}
+          addCommas={addCommas}
+        />
       </Grid>
     </Grid>
   );
